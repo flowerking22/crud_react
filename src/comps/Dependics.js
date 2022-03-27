31e4063710a8a96057f06db534fd18960cb90f54
flowerking30 <100342127+flowerking30@users.noreply.github.com>
2022-03-27 14:53:36 +0530

    Add files via upload

---
+++ src/comps/Dependics.js
@@ -0,0 +1,52 @@
+import React from 'react';
+export default function Depensics() {
+    return (
+        <div>
+            <ol class="list-group list-group-numbered">
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">react</div>
+                        A JavaScript library for building user interfaces<br />
+                        React. js is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications. It's used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components
+                    </div>
+                    <span class="badge bg-primary rounded-pill">17.0.2</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">
+                            react-dom</div>
+                        The react-dom package provides DOM-specific methods that can be used at the top level of your app and as an escape hatch to get outside of the React model </div>
+                    <span class="badge bg-primary rounded-pill">17.0.2</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">react-router-dom</div>
+                        React Router DOM is an npm package that enables you to implement dynamic routing in a web app. It allows you to display pages and allow users to navigate them. It is a fully-featured client and server-side routing library for React
+                    </div>
+                    <span class="badge bg-primary rounded-pill">6.2.2</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">bootstrap</div>
+                        Bootstrap is a potent front-end framework used to create modern websites and web apps. It's open-source and free to use, yet features numerous HTML and CSS templates for UI interface elements such as buttons and forms. Bootstrap also supports JavaScript extensions.
+                    </div>
+                    <span class="badge bg-primary rounded-pill">5.1.3</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">font-awesome</div>
+                        Font Awesome is a widely-used icon set that gives you scalable vector images that can be customized with CSS. With over 1,600 icons in the free set, you should be able to find an icon to suit your needs.
+                    </div>
+                    <span class="badge bg-primary rounded-pill">4.0.1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">react-router-dom</div>
+                        Cras justo odio
+                    </div>
+                    <span class="badge bg-primary rounded-pill">6.2.2</span>
+                </li>
+            </ol>
+        </div>
+    )
+}
